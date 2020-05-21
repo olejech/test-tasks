73fb4684d12f03c8b933449e8ea17e7c666bae33
olejech <olejech@gmail.com>
2020-05-21 11:45:53 +0300

    Added modal

--- tr-logic/src/views/Note.vue
+++ tr-logic/src/views/Note.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="note">
-    <button class="btn button-add">Добавить заметку</button>
+    <button class="btn button-add" @click="openModal">Добавить заметку</button>
     <div class="note-wrap">
       <h1>Поиск работы [id {{this.$route.params.id}}]</h1>
       <ul class="list">
         <li class="item">
           <a href="#" class="link">Найти работу</a>
+          <button class="btn button-change" @click="changeTodo">Изменить</button>
           <button class="btn button-remove" @click="removeTodo">Удалить</button>
         </li>
         <li class="item">
@@ -26,14 +27,44 @@
         </li>
       </ul>
     </div>
+
+    <div class="modal" :class="{active: isOpenModal}">
+      <h2>Добавить новую заметку</h2>
+      <label for="note" class="modal__label">Название заметки</label>
+      <input type="text" name="note" id="note" class="modal__input" />
+      <label for="todos" class="modal__label">Список задач (каждая с новой строки)</label>
+      <textarea name="todos" id cols="20" rows="5" class="modal__textarea"></textarea>
+      <button class="btn button-cancel" @click="closeModal">Отмена</button>
+      <button class="btn button-add" @click="addNote">Добавить</button>
+    </div>
+    <div class="overlay" :class="{active: isOpenModal}"></div>
   </div>
 </template>
 
 <script>
 export default {
   name: 'Note',
+  data() {
+    return {
+      isOpenModal: false,
+    };
+  },
   methods: {
+    openModal() {
+      console.log('click');
+
+      this.isOpenModal = true;
+    },
+    closeModal() {
+      this.isOpenModal = false;
+    },
     removeTodo() {
+      console.log('click');
+    },
+    changeTodo() {
+      console.log('click');
+    },
+    addNote() {
       console.log('click');
     },
   },
@@ -66,6 +97,16 @@ export default {
 .button-add {
   background: #42b983;
   color: white;
+}
+.button-change {
+  background: #e4c92e;
+  color: white;
+  margin-right: 10px;
+}
+.button-cancel {
+  background: darkRed;
+  color: white;
+  margin-right: 10px;
 }
 .note {
   max-width: 600px;
