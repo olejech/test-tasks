d11879c2134c90a41aaea8023a0fb3caa10d7cab
olejech <olejech@gmail.com>
2020-06-23 12:57:18 +0300

    Добавлен common stylus, pritter

--- arenum/src/App.vue
+++ arenum/src/App.vue
@@ -5,23 +5,14 @@
 </template>
 
 <script>
-import HelloWorld from "./components/HelloWorld.vue";
+import HelloWorld from './components/HelloWorld.vue'
 
 export default {
-  name: "App",
+  name: 'App',
   components: {
     HelloWorld
   }
-};
+}
 </script>
 
-<style lang="stylus">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
+<style lang="stylus"></style>
