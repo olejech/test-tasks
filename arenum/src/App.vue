b28d6a56884db53e7a832f83e24e247ef369d635
olejech <olejech@gmail.com>
2020-06-23 15:47:58 +0300

    Добавлен GameItem, подключен Axios, Moment

--- arenum/src/App.vue
+++ arenum/src/App.vue
@@ -1,16 +1,43 @@
 <template>
   <div class="container" id="app">
-    <game-item />
+    <div v-for="game in games" :key="game.tournamentCode">
+      <game-item
+        :image="game.tournamentData.cardImage"
+        :date="game.tournamentData.startedAt"
+        :paymentType="game.tournamentData.paymentType"
+        :name="game.tournamentData.name"
+        :gameMode="game.tournamentData.tournamentType.gameMode"
+        :participateCount="game.tournamentData.participateCount"
+        :maxUsers="game.tournamentData.maxUsers"
+        :prizeTable="game.tournamentData.prizeTable"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+import axios from './plugins/axios.js'
 import GameItem from './components/GameItem.vue'
 
 export default {
   name: 'App',
   components: {
     GameItem
+  },
+  data() {
+    return {
+      games: []
+    }
+  },
+  async mounted() {
+    try {
+      const res = await axios.get(
+        '?start=0&gameCode=f533d4be-5b8e-11e9-8647-d663bd873d93&max=15&includePaid=true'
+      )
+      this.games = res.data
+    } catch (error) {
+      console.log('mounted -> error', error)
+    }
   }
 }
 </script>
@@ -23,6 +50,5 @@ export default {
   margin: 0 auto;
   padding: 18px 20px;
   background: $black;
-  height: 100vh;
 }
 </style>
--- arenum/src/components/GameItem.vue
+++ arenum/src/components/GameItem.vue
@@ -1,11 +1,119 @@
 <template>
-  <div class="game-item">123</div>
+  <div class="item">
+    <img class="image" :src="imageCard" :alt="name" />
+    <div class="item-info">
+      <div class="date">{{ dateFormat }}</div>
+      <img class="icon" :src="icon" alt />
+      <span class="name">{{ name }}</span>
+      <div class="params">
+        <span class="mode">{{ gameMode }}</span>
+        <span class="participate">{{ participateCount }} / {{ maxUsers }}</span>
+        <span>{{ prizeTotal }}</span>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
+import moment from 'moment'
+
 export default {
-  name: 'HelloWorld'
+  name: 'GameItem',
+  props: {
+    image: {
+      type: String
+    },
+    date: {
+      type: String
+    },
+    paymentType: {
+      type: String
+    },
+    name: {
+      type: String
+    },
+    gameMode: {
+      type: String
+    },
+    participateCount: {
+      type: Number
+    },
+    maxUsers: {
+      type: Number
+    },
+    prizeTable: {
+      type: Array
+    }
+  },
+  computed: {
+    imageCard() {
+      return this.image
+        ? this.image + '?width=300&height=300'
+        : 'https://via.placeholder.com/60/60'
+    },
+    icon() {
+      return this.paymentType === 'free'
+        ? '/images/free.svg'
+        : '/images/ticket.svg'
+    },
+    prizeTotal() {
+      return this.prizeTable.reduce((acc, current) => acc + current)
+    },
+    dateFormat() {
+      return moment(this.date)
+        .lang('ru')
+        .format('DD MMMM YYYY, в h:mm')
+    }
+  }
 }
 </script>
 
-<style scoped lang="stylus"></style>
+<style scoped lang="stylus">
+@import '../assets/stylus/common/vars';
+
+.item {
+  display: flex;
+  padding: 18px 0;
+  border-bottom: 1px solid lighten($black, 15%);
+  transition: $duration;
+
+  &:hover {
+    border-bottom: 1px solid lighten($black, 50%);
+    cursor: pointer;
+  }
+}
+
+.image {
+  max-width: 60px;
+  margin-right: 16px;
+}
+
+.date {
+  margin-bottom: 2px;
+  font-size: $fontSizeSm;
+  line-height: $lineHeightSm;
+  color: $white;
+}
+
+.name {
+  font-size: $fontSizeLg;
+  line-height: $lineHeightLg;
+  color: $white;
+}
+
+.icon {
+  margin-right: 6px;
+}
+
+.mode, .participate {
+  position: relative;
+  margin-right: 15px;
+
+  &:after {
+    content: '\2022';
+    position: absolute;
+    top: 0;
+    right: -10px;
+  }
+}
+</style>
